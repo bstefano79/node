95d44507c7e4d3d59fcf2077c7b226f0a25f3248
Stefano Belli <56109150+bstefano79@users.noreply.github.com>
2022-05-04 13:54:13 +0200

    prototype

---
+++ prototype.js
@@ -0,0 +1,12 @@
+var v=[1,2,3];
+//v.myfunction();  output --> error v.myfunction is not a function
+
+Array.prototype.f1 = function () {console.log(this); return 0;};
+Array.prototype.f2 = () => {console.log(this); return 0;};
+
+v.f1();
+v.f2();
+
+/*Array.prototype.myfunction = (f) => {
+    this.map((x)=>f(x));
+}*/
