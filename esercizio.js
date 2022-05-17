8c5204dc4ce002dcc5528e0c90d40e5b0f22a260
Stefano Belli <56109150+bstefano79@users.noreply.github.com>
2022-05-17 15:07:07 +0200

    correzione dell esercizio

--- esercizio.js
+++ esercizio.js
@@ -6,22 +6,24 @@ let v = [
     { name:"persona3", costi:1234, spese:2345},
 ];
 
-
-
 let risultato = v.reduce((res,p)=>{
     //nel vettore res è presente la chiave con il nome?
-    if(res[p.name]){
+    if(res.key[p.name]!=null){
         //si è presente aggiungo costi e spese
-        res[p.name].costi+=p.costi;
-        res[p.name].spese+=p.spese;
+        let key = res.key[p.name];
+        res.value[key].costi+=p.costi;
+        res.value[key].spese+=p.spese;
     }else{
         //non è presente assegno l'oggetto alla posizione col nome
-        res[p.name] = p;
+        let key = res.value.length;
+        res.key[p.name] = key;
+        res.value[key] = p;
     }
     //ritorno il vettore
+
     return res;
     
-},{});
+},{key:{}, value : []});
 //il vettore risultato lo inizializzo a vettore vuoto []
 
-console.log(risultato);
+//console.log(risultato.value);
