0892a7717677e3fcea5d7e6ed7deb97ce26df426
Stefano Belli <56109150+bstefano79@users.noreply.github.com>
2022-05-17 09:50:35 +0200

    commenti esercizio gruppo facebook

--- esercizio.js
+++ esercizio.js
@@ -7,14 +7,19 @@ let v = [
 ];
 
 let risultato = v.reduce((res,p)=>{
+    //nel vettore res è presente la chiave con il nome?
     if(res[p.name]){
+        //si è presente aggiungo costi e spese
         res[p.name].costi+=p.costi;
         res[p.name].spese+=p.spese;
     }else{
+        //non è presente assegno la l'oggetto alla posizione col nome
         res[p.name] = p;
     }
+    //ritorno il vettore
     return res;
     
 },[]);
+//il vettore risultato lo inizializzo a vettore vuoto []
 
 console.log(risultato);
